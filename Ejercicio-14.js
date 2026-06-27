eb75cba432f88d5688f4323ef84a6fd2fe1ad638
Giscarlys Chavez <giskcv@gmail.com>
2026-06-27 22:44:35 +0200

    Ejercicios3

---
+++ Ejercicio-14.js
@@ -0,0 +1,29 @@
+const words = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code"
+];
+
+function repeatCounter(list) {
+  let result = {};
+
+  for (let i = 0; i < list.length; i++) {
+    if (result[list[i]]) {
+      result[list[i]]++;
+    } else {
+      result[list[i]] = 1;
+    }
+  }
+
+  return result;
+}
+
+console.log(repeatCounter(words));
